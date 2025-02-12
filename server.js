5e804e5b360991720889ae83098de070f0fbd0b5
sajid210177 <161884984+sajid210177@users.noreply.github.com>
2025-02-13 00:07:55 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express"
+import cors from "cors"
+import 'dotenv/config'
+
+//app config
+
+const app = express()
+const port = process.env.PORT || 4000
+
+//middleware
+
+app.use(express.json())
+app.use(cors())
+
+//api end points
+
+app.get('/',(req,res)=>{
+    res.send("Api Working")
+})
+
+app.listen(port, ()=> console.log('Server is running on PORT : '+ port))
