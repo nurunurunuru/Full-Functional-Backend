082e4312f8debfeb144b2abce796e244124fbb7f
sajid210177 <161884984+sajid210177@users.noreply.github.com>
2025-02-21 01:00:49 +0600

    commit message

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import connectDB from "./config/mongodb.js"
 import connectCloudinary from "./config/cloudinary.js"
 import userRouter from "./routes/userRoute.js"
 import productRouter from "./routes/productRoute.js"
+import cartRouter from "./routes/cartRoute.js"
 
 //app config
 
@@ -22,6 +23,7 @@ app.use(cors())
 //api end points
 app.use('/api/user', userRouter)
 app.use('/api/product', productRouter)
+app.use('/api/cart', cartRouter)
 
 app.get('/',(req,res)=>{
     res.send("Api Working")
