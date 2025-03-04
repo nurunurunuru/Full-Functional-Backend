73469d26392f0365c20b3292dab5df39179f300d
sajid210177 <161884984+sajid210177@users.noreply.github.com>
2025-03-04 17:26:39 +0600

    commit message

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -33,12 +33,27 @@ const placeOrderStripe = async (req,res)=>{
 }
 
 //All Orders data for admin pannel
-const allOrders = async (req,res)=>{
-    
-}
+const allOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find().populate('userId', 'name email'); // Fetch all orders with user details
+    res.json({ success: true, orders });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
 //All Orders data for FrontEnd
 const userOrders = async (req,res)=>{
+  try {
+    const {userId} = req.body
 
+    const orders = await orderModel.find({userId})
+    res.json({success:true, orders})
+  } catch (error) {
+    console.log(error)
+    res.json({success:false, message:error.message})
+  }
 }
 //All Orders status for admin pannel
 const updateStatus = async (req,res)=>{
